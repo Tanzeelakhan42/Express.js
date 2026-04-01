f1043809f186e9a99213f2b26fc5d3f520a18b14
Tanzeelakhan42 <tanzeelakhan42@gmail.com>
2026-04-01 19:34:43 +0530

    add GET /api/data/:id route to fetch user by id

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const data = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 8000;
 
+// app.get("/data", (req, res) => {
+//   const html = `<ul>${data.map((item) => `<li>${item.first_name}</li>`).join("")}</ul>`;
+//   return res.send(html);
+// });
 app.get("/api/data", (req, res) => {
   return res.json(data);
 });
-app.get("/data", (req, res) => {
-  const html = `<ul>${data.map((item) => `<li>${item.first_name}</li>`).join("")}</ul>`;
-  return res.send(html);
+app.get("/api/data/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = data.find((item) => item.id === id);
+  return res.json(user);
 });
-
 app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));

ef9d2af5bb725c2bd0569c741c84d7332fad11ba
Tanzeelakhan42 <tanzeelakhan42@gmail.com>
2026-04-01 14:59:42 +0530

    add GET routes to serve mock data as JSON and HTML

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ const data = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 8000;
 
+app.get("/api/data", (req, res) => {
+  return res.json(data);
+});
+app.get("/data", (req, res) => {
+  const html = `<ul>${data.map((item) => `<li>${item.first_name}</li>`).join("")}</ul>`;
+  return res.send(html);
+});
+
 app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
